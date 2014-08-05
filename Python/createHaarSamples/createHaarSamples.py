34a4cd32ceffbaa847bd741537da9891ffb4626d
Thiago Hersan <thiago.hersan+github@gmail.com>
2014-08-06 01:15:23 +0200

    runs opencv_createsamples command

--- Python/createHaarSamples/createHaarSamples.py
+++ Python/createHaarSamples/createHaarSamples.py
@@ -1,4 +1,4 @@
-from os import listdir, path, makedirs
+from os import listdir, path, makedirs, remove
 from re import sub
 from subprocess import call
 from time import sleep
@@ -14,25 +14,26 @@ if __name__=="__main__":
         makedirs(PATH_SAMPLE_IMAGES)
 
     # get list of negative images
+    negImageCollectionFilename = PATH_NEG_IMAGES+".txt"
+    negImageCollectionFile = open(negImageCollectionFilename, "w")
     negImageFilenames = [f for f in listdir(PATH_NEG_IMAGES) if path.isfile(path.join(PATH_NEG_IMAGES,f)) and f.lower().endswith("jpg")]
-    negImageCollectionFile = open(PATH_NEG_IMAGES+".txt", "w")
     for f in negImageFilenames:
         negImageCollectionFile.write(PATH_NEG_IMAGES.split('/')[-1]+"/"+f)
         negImageCollectionFile.write("\n")
 
-    # get list of images
+    # get list of positive image files
     posImageFilenames = [f for f in listdir(PATH_POS_IMAGES) if path.isfile(path.join(PATH_POS_IMAGES,f))]
-
     # run the create command for each image
     for f in posImageFilenames:
         cmd = "opencv_createsamples"
         cmd += " -img "+path.join(PATH_POS_IMAGES,f)
-        cmd += " -bg "+PATH_NEG_IMAGES+".txt"
+        cmd += " -bg "+negImageCollectionFilename
         cmd += " -info "+path.join(PATH_SAMPLE_IMAGES,sub("(?i)jpg","txt",f))
         cmd += " -num 10 -maxxangle 0.0 -maxyangle 0.0 -maxzangle 0.4 -bgcolor 255 -bgthresh 8 -w 48 -h 48"
-        print cmd
+        call(cmd, shell=True)
 
         '''
         opencv_createsamples -img positive-clean-00_cropped/IMG_1620.JPG -bg negative-tutorial-haartraining.txt -info 1620_foo.txt -num 10 
         -maxxangle 0.0 -maxyangle 0.0 -maxzangle 0.4 -bgcolor 255 -bgthresh 8 -w 48 -h 48
         '''
+    remove(negImageCollectionFilename)
