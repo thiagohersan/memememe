a7bebab20c1e214831491cd08ef85d83d54c7b2d
Thiago Hersan <thiago.hersan+github@gmail.com>
2014-08-06 01:40:50 +0200

    uses check_call and adds delay to keep opencv calls from clobbering each other

--- Python/createHaarSamples/createHaarSamples.py
+++ Python/createHaarSamples/createHaarSamples.py
@@ -1,6 +1,6 @@
 from os import listdir, path, makedirs, remove
 from re import sub
-from subprocess import call
+from subprocess import check_call
 from time import sleep
 from random import random
 
@@ -20,20 +20,24 @@ if __name__=="__main__":
     for f in negImageFilenames:
         negImageCollectionFile.write(PATH_NEG_IMAGES.split('/')[-1]+"/"+f)
         negImageCollectionFile.write("\n")
+    negImageCollectionFile.close()
 
     # get list of positive image files
     posImageFilenames = [f for f in listdir(PATH_POS_IMAGES) if path.isfile(path.join(PATH_POS_IMAGES,f))]
     # run the create command for each image
     for f in posImageFilenames:
-        cmd = "opencv_createsamples"
-        cmd += " -img "+path.join(PATH_POS_IMAGES,f)
-        cmd += " -bg "+negImageCollectionFilename
-        cmd += " -info "+path.join(PATH_SAMPLE_IMAGES,sub("(?i)jpg","txt",f))
-        cmd += " -num 10 -maxxangle 0.0 -maxyangle 0.0 -maxzangle 0.4 -bgcolor 255 -bgthresh 8 -w 48 -h 48"
-        call(cmd, shell=True)
+        check_call(["opencv_createsamples",
+            "-img", path.join(PATH_POS_IMAGES,f),
+            "-bg", negImageCollectionFilename,
+            "-info", path.join(PATH_SAMPLE_IMAGES,sub("(?i)jpg","txt",f)),
+            "-num", "10",
+            "-maxxangle", "0.0",
+            "-maxyangle", "0.0",
+            "-maxzangle", "0.4",
+            "-bgcolor", "255",
+            "-bgthresh", "8",
+            "-w", "48",
+            "-h", "48"])
+        sleep(1)
 
-        '''
-        opencv_createsamples -img positive-clean-00_cropped/IMG_1620.JPG -bg negative-tutorial-haartraining.txt -info 1620_foo.txt -num 10 
-        -maxxangle 0.0 -maxyangle 0.0 -maxzangle 0.4 -bgcolor 255 -bgthresh 8 -w 48 -h 48
-        '''
     remove(negImageCollectionFilename)
