b37b792813aeaaffa1a46fcea3b4d245842757f7
Thiago Hersan <thiago.hersan+github@gmail.com>
2014-08-20 18:22:08 +0200

    removes explicit range-based for-loops. cleans up unecessary A[] variable.

--- Python/selfieMemememe/stewartPlatformMath.py
+++ Python/selfieMemememe/stewartPlatformMath.py
@@ -22,59 +22,54 @@ class StewartPlatformMath:
         self.platformJoint = []
         self.q = []
         self.l = []
-        self.A = []
         self.alpha = []
         self.baseRadius = scale*StewartPlatformMath.SCALE_BASE_RADIUS
         self.platformRadius = scale*StewartPlatformMath.SCALE_PLATFORM_RADIUS
         self.hornLength = scale*StewartPlatformMath.SCALE_HORN_LENGTH
         self.legLength = scale*StewartPlatformMath.SCALE_LEG_LENGTH;
 
-        for i in range(6):
-            mx = self.baseRadius*cos(radians(self.baseAngles[i]))
-            my = self.baseRadius*sin(radians(self.baseAngles[i]))
-            self.baseJoint.insert(i, Vector3(mx, my))
+        for angle in self.baseAngles:
+            mx = self.baseRadius*cos(radians(angle))
+            my = self.baseRadius*sin(radians(angle))
+            self.baseJoint.append(Vector3(mx, my))
 
-        for i in range(6):
-            mx = self.platformRadius*cos(radians(self.platformAngles[i]))
-            my = self.platformRadius*sin(radians(self.platformAngles[i]))
-            self.platformJoint.insert(i, Vector3(mx, my))
-            self.q.insert(i,Vector3())
-            self.l.insert(i,Vector3())
-            self.A.insert(i,Vector3())
-            self.alpha.insert(i,0)
+        for angle in self.platformAngles:
+            mx = self.platformRadius*cos(radians(angle))
+            my = self.platformRadius*sin(radians(angle))
+            self.platformJoint.append(Vector3(mx, my))
+
+        self.q = [Vector3()]*len(self.platformAngles)
+        self.l = [Vector3()]*len(self.platformAngles)
+        self.alpha = [0]*len(self.beta)
 
     def calcQ(self):
-        for i in range(6):
+        for (i,joint) in enumerate(self.platformJoint):
             # rotation
-            self.q[i].x = (cos(self.rotation.z)*cos(self.rotation.y)*self.platformJoint[i].x +
-                (-sin(self.rotation.z)*cos(self.rotation.x)+cos(self.rotation.z)*sin(self.rotation.y)*sin(self.rotation.x))*self.platformJoint[i].y +
-                (sin(self.rotation.z)*sin(self.rotation.x)+cos(self.rotation.z)*sin(self.rotation.y)*cos(self.rotation.x))*self.platformJoint[i].z)
+            self.q[i].x = (cos(self.rotation.z)*cos(self.rotation.y)*joint.x +
+                (-sin(self.rotation.z)*cos(self.rotation.x)+cos(self.rotation.z)*sin(self.rotation.y)*sin(self.rotation.x))*joint.y +
+                (sin(self.rotation.z)*sin(self.rotation.x)+cos(self.rotation.z)*sin(self.rotation.y)*cos(self.rotation.x))*joint.z)
 
-            self.q[i].y = (sin(self.rotation.z)*cos(self.rotation.y)*self.platformJoint[i].x +
-                (cos(self.rotation.z)*cos(self.rotation.x)+sin(self.rotation.z)*sin(self.rotation.y)*sin(self.rotation.x))*self.platformJoint[i].y +
-                (-cos(self.rotation.z)*sin(self.rotation.x)+sin(self.rotation.z)*sin(self.rotation.y)*cos(self.rotation.x))*self.platformJoint[i].z)
+            self.q[i].y = (sin(self.rotation.z)*cos(self.rotation.y)*joint.x +
+                (cos(self.rotation.z)*cos(self.rotation.x)+sin(self.rotation.z)*sin(self.rotation.y)*sin(self.rotation.x))*joint.y +
+                (-cos(self.rotation.z)*sin(self.rotation.x)+sin(self.rotation.z)*sin(self.rotation.y)*cos(self.rotation.x))*joint.z)
 
             self.q[i].z = (-sin(self.rotation.y)*self.platformJoint[i].x +
-                cos(self.rotation.y)*sin(self.rotation.x)*self.platformJoint[i].y +
-                cos(self.rotation.y)*cos(self.rotation.x)*self.platformJoint[i].z)
+                cos(self.rotation.y)*sin(self.rotation.x)*joint.y +
+                cos(self.rotation.y)*cos(self.rotation.x)*joint.z)
 
             # translation
             self.q[i] += (self.translation + self.initialHeight)
             self.l[i] = self.q[i] - self.baseJoint[i]
 
     def calcAlpha(self):
-        for i in range(6):
+        for (i,joint) in enumerate(self.baseJoint):
             L = self.l[i].magnitudeSquared()-(self.legLength**2)+(self.hornLength**2)
-            M = 2*self.hornLength*(self.q[i].z-self.baseJoint[i].z)
-            N = 2*self.hornLength*(cos(self.beta[i])*(self.q[i].x-self.baseJoint[i].x) + sin(self.beta[i])*(self.q[i].y-self.baseJoint[i].y))
+            M = 2*self.hornLength*(self.q[i].z-joint.z)
+            N = 2*self.hornLength*(cos(self.beta[i])*(self.q[i].x-joint.x) + sin(self.beta[i])*(self.q[i].y-joint.y))
             try:
                 self.alpha[i] = asin(L/sqrt(M*M+N*N)) - atan2(N,M)
             except ValueError:
                 self.alpha[i] = float('NaN')
-
-            self.A[i].x = self.hornLength*cos(self.alpha[i])*cos(self.beta[i]) + self.baseJoint[i].x
-            self.A[i].y = self.hornLength*cos(self.alpha[i])*sin(self.beta[i]) + self.baseJoint[i].y
-            self.A[i].z = self.hornLength*sin(self.alpha[i]) + self.baseJoint[i].z
 
     def applyTranslationAndRotation(self, t=Vector3(), r=Vector3()):
         self.rotation = r.copy()
