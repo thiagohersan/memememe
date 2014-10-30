8a98e668d1a4258a1681718f4392bfe7f0bf03b9
Thiago Hersan <thiago.hersan+github@gmail.com>
2014-10-29 20:36:41 -0400

    decreases LOOKING timeout

--- Android/selfieMemememe/src/me/memememe/selfie/MemememeActivity.java
+++ Android/selfieMemememe/src/me/memememe/selfie/MemememeActivity.java
@@ -56,7 +56,7 @@ public class MemememeActivity extends Activity implements CvCameraViewListener2 
 
     private static final int TIMEOUT_SCANNING = 10000;
     private static final int TIMEOUT_REFLECTING = 5000;
-    private static final int TIMEOUT_MAKING_NOISE_LOOKING = 30000;
+    private static final int TIMEOUT_MAKING_NOISE_LOOKING = 15000;
     private static final int PERIOD_MAKING_NOISE_LOOKING = 2000;
     private static final int TIMEOUT_MAKING_NOISE_REFLECTING = 2000;
     private static final int TIMEOUT_FLASHING = 4000;
