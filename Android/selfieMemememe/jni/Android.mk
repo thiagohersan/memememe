d6602a0c9e42406097240cfe844685d893f6da71
Thiago Hersan <thiago.hersan+github@gmail.com>
2016-01-17 21:08:11 +0000

    updates android project files

--- Android/selfieMemememe/jni/Android.mk
+++ Android/selfieMemememe/jni/Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 #OPENCV_CAMERA_MODULES:=off
 #OPENCV_INSTALL_MODULES:=off
 #OPENCV_LIB_TYPE:=SHARED
-include ../../../../../../Dev/openCV/OpenCV-2.4.9-android-sdk/sdk/native/jni/OpenCV.mk
+include ../../../../../../Dev/OpenCV-2.4.9-android-sdk/sdk/native/jni/OpenCV.mk
 
 LOCAL_SRC_FILES  := DetectionBasedTracker_jni.cpp
 LOCAL_C_INCLUDES += $(LOCAL_PATH)
