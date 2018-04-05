4422f077066d9912bd200002257ce7e15c3a1a48
Radames Ajna <radamajna@gmail.com>
2018-04-05 16:12:18 -0700

    counter bigger hours for thursdays

--- bashrc_run.sh
+++ bashrc_run.sh
@@ -4,13 +4,14 @@ if [ $isrun -lt 1 ]
 then
     cd /home/pi/Dev/memememe/Python/selfieMemememe
     cnt=0
-    while [ $cnt -lt 50 ]
+    if [[ $(date +%u) -eq 4 ]] ; then maxcnt=132; else maxcnt=84; fi
+    while [ $cnt -lt $maxcnt ]
     do
 	sudo python selfieMemememe.py &
 	sleep 1
 	killsudopid=$!
 	killpythonpid=`ps -u root | awk '/python/{print $1}'`
-	sleep 600
+	sleep 300
 	sudo kill -9 $killpythonpid
 	sudo kill -9 $killsudopid
 	let cnt=cnt+1
